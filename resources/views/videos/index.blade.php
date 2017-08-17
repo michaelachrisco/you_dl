060adf76f1fbdc5ca59d3970297f0e67d60e58a8
Michael Chrisco <michaelachrisco@gmail.com>
2017-08-17 11:13:26 -0700

    Import Email and Video Service.

--- resources/views/videos/index.blade.php
+++ resources/views/videos/index.blade.php
@@ -5,12 +5,16 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">{{ Auth::user()->name }}s Videos</div>
+                    <div class="panel-heading">{{ Auth::user()->name }}s Videos
+                        <a class="btn btn-primary">
+                            <span class="glyphicon glyphicon-plus" />
+                        </a>
+                    </div>
                         <div class="row">
                             @foreach($videos as $video)
                                 <div class="col-xs-18 col-sm-6 col-md-3">
                                     <div class="thumbnail">
-                                        <img src="http://placehold.it/500x250/EEE">
+                                        <a href=" {{ $video->link }}" ><img src="http://placehold.it/500x250/EEE"></a>
                                         <div class="caption">
                                             <h4>{{ $video->name }}</h4>
                                             <p>{{ $video->description }}</p>
